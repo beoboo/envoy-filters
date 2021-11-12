d8e93dd2d3023b54b5d4fb44f78deeb1f73ef8ad
Pietro Atzeni <pietrino.atzeni@moneyfarm.com>
2021-11-12 13:00:44 +0100

    Testing filter

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ use proxy_wasm as wasm;
 use proxy_wasm::traits::{Context, HttpContext};
 use proxy_wasm::types::{Action, LogLevel};
 
+use log::info;
+
 #[no_mangle]
 pub fn _start() {
     wasm::set_log_level(LogLevel::Trace);
@@ -12,8 +14,8 @@ pub fn _start() {
     wasm::set_http_context(
         |context_id, root_context_id| -> Box<dyn HttpContext> {
             Box::new(MyContext {
-                context_id: context_id,
-                root_context_id: root_context_id,
+                context_id,
+                root_context_id,
             })
         }
     );
@@ -24,8 +26,7 @@ struct MyContext {
     root_context_id: u32,
 }
 
-impl MyContext {
-}
+impl MyContext {}
 
 impl Context for MyContext {
     fn on_done(&mut self) -> bool {
@@ -34,28 +35,30 @@ impl Context for MyContext {
 }
 
 impl HttpContext for MyContext {
-    fn on_http_request_headers(&mut self, num_headers: usize) -> Action {
+    fn on_http_request_headers(&mut self, _num_headers: usize) -> Action {
+        info!("Request from WASM");
         Action::Continue
     }
 
-    fn on_http_request_trailers(&mut self, num_trailers: usize) -> Action {
+    fn on_http_request_body(&mut self, _body_size: usize, _stream_end: bool) -> Action {
         Action::Continue
     }
 
-    fn on_http_request_body(&mut self, body_size: usize, stream_end: bool) -> Action {
+    fn on_http_request_trailers(&mut self, _num_trailers: usize) -> Action {
         Action::Continue
     }
 
-    fn on_http_response_headers(&mut self, num_headers: usize) -> Action {
+    fn on_http_response_headers(&mut self, _num_headers: usize) -> Action {
+        info!("Response from WASM");
         self.add_http_response_header("x-my-custom-header", "hello world");
         Action::Continue
     }
 
-    fn on_http_response_trailers(&mut self, num_trailers: usize) -> Action {
+    fn on_http_response_body(&mut self, _body_size: usize, _stream_end: bool) -> Action {
         Action::Continue
     }
 
-    fn on_http_response_body(&mut self, body_size: usize, stream_end: bool) -> Action {
+    fn on_http_response_trailers(&mut self, _num_trailers: usize) -> Action {
         Action::Continue
     }
 }
